8ab0b4d763b795fc7c9c24f76f2c2c815a03fa7d
mmhernandez <mmdangelo11@gmail.com>
2022-12-16 17:09:03 -0700

    updates

---
+++ script.js
@@ -0,0 +1,9 @@
+var currentTime = new Date();
+var currentHour = currentTime.getHours();
+var currentMinute = currentTime.getMinutes();
+var currentSecond = currentTime.getSeconds();
+
+console.log(currentHour);
+console.log(currentMinute);
+console.log(currentSecond);
+
